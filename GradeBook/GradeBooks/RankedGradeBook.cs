d61aaa2137f8e5754a8e9d0675aaf3e100778741
NikolaPasic <nikola.pasic@live.com>
2019-04-08 22:29:57 +0800

    GetLetterGrade Override

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,32 +1,34 @@
-﻿namespace GradeBook.GradeBooks
+﻿using System;
+using System.Linq;
+using GradeBook.Enums;
+namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
-            Type =Enums.GradeBookType.Ranked;
+            Type = GradeBookType.Ranked;
         }
 
-        public override GetLetterGrade(double averageGrade)
+        public override char GetLetterGrade(double averageGrade)
         {
-            if(Students.Count < 5)
-                throw new InvalidOperationExceptoin("Rank grading requires at least 5 students");
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked grading requres at least 5 students");
 
-            var treshold = (int)math.Ceiling(Students.Count * 0.2);
+            var trashold = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
-            if (grades[treshold - 1] <= averageGrade)
+            if (grades[trashold - 1] <= averageGrade)
                 return 'A';
-            else if (grades[(treshold * 2) - 1] <= averageGrade)
+            else if (grades[(trashold * 2) - 1] <= averageGrade)
                 return 'B';
-            else if (grades[(treshold * 3) - 1] <= averageGrade)
+            else if (grades[(trashold * 3) - 1] <= averageGrade)
                 return 'C';
-            else if (grades[(treshold * 4) - 1] <= averageGrade)
+            else if (grades[(trashold * 4) - 1] <= averageGrade)
                 return 'D';
             else
                 return 'F';
-                return base.GetLetterGrade(averageGrade);
+ 
         }
     }
 }
-
