4173e148b97737aeeea6ff058e9fdb6a55cb0677
NikolaPasic <nikola.pasic@live.com>
2019-04-09 11:29:51 +0800

    Spelling Fix1#

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -13,7 +13,7 @@ namespace GradeBook.GradeBooks
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
-                throw new InvalidOperationException("Ranked grading reqires at least 5 students");
+                throw new InvalidOperationException("Ranked grading requires at least 5 students");
 
             var trashold = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
@@ -33,21 +33,22 @@ namespace GradeBook.GradeBooks
         {
             if (Students.Count < 5)
             {
-                Console.WriteLine("Ranked grading requires at least 5 studens");
+                Console.WriteLine("Ranked grading requires at least 5 students");
                 return;
             }
-
             base.CalculateStatistics();
         }
         public override void CalculateStudentStatistics(string name)
         {
-            if (Students.Count < 5)
+            if(Students.Count < 5)
             {
-                Console.WriteLine("Ranked grading requires at least 5 studens");
+                Console.WriteLine("Ranked grading requires at least 5 students");
                 return;
             }
 
             base.CalculateStudentStatistics(name);
         }
     }
+
 }
+
